1c37f201b47734ecbc8e625cc7c3dd1730aa6e72
samdarbonne <samdarbonne@gmail.com>
2016-08-26 15:11:47 -0700

    makes a random colored circle on the screen

---
+++ app.js
@@ -0,0 +1,41 @@
+// push me, and then just touch me, till i can get my satisfaction
+
+var randomr = 10;
+var gameboardsize;
+
+randomColorValue = function(){
+	return (Math.round(Math.random() * 254));
+}
+
+randomx = function(){
+	return Math.round(Math.random() * 200);
+}
+
+randomy = function(){
+	return Math.round(Math.random() * 200);
+}
+function randomColor(){
+	red = randomColorValue();
+	green = randomColorValue();
+	blue = randomColorValue();
+	return('rgb(' + red + ', ' + green + ', ' + blue + ')');
+}
+
+var svgholder = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+var Bubble = function(radius){
+	this.xpos = randomx();
+	this.ypos = randomy();
+	this.radius = randomr;
+	this.color = randomColor();
+	this.place = function(){
+		var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");;
+		circle.setAttribute('cy', this.ypos);
+		circle.setAttribute('cx', this.xpos);
+		circle.setAttribute('r', this.radius);
+		circle.setAttribute('fill', this.color);
+		$('#gameboard').append(circle);
+	}
+}
+
+var bubble1 = new Bubble(10);
+bubble1.place();
