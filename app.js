cad9c0ef1fd978a5b5d899009a422dccb15e4c2d
samdarbonne <samdarbonne@gmail.com>
2016-08-26 21:57:05 -0700

    adds click listener to bubbles

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
-// push me, and then just touch me, till i can get my satisfaction
 
-// on ddoc ready 
-// $('.circles').on(click calll){
-//	this.remove
-//}
 
-// $( document ).ready(function() {
-//     $('.circles').on('click', function(){
-//     	this.parentNode.removeChild(this);
-//     })
-// })
-function removal(){
-	console.log('removing!')
-}
+
+
+$( document ).ready(function() {
+    gameStart();
+})
+
+
+
 
 function countCircles(){
-	if(document.getElementById('gameboard').childNodes.length === 0){
+	if(gameboard.childNodes.length === 0){
 		console.log('good job you killed all the bubbles!')
 	}
 }
-var randomr = 10;
-var gameboardsize;
+
+
 var gameboard = document.getElementById('gameboard');
 
-randomColorValue = function(){
-	return (Math.round(Math.random() * 254));
-}
-
-randomx = function(){
+function randomx(){
 	return (Math.round(Math.random() * 460) + 30);
 }
 
-randomy = function(){
+function randomy(){
 	return (Math.round(Math.random() * 460) + 30);
 }
+
+function randomColorValue(variety, offset){
+	return (Math.round(Math.random() * variety) + offset);
+}
+
+//chose a semi-random color value for each color component, then concatenate them into an rgb color code
 function randomColor(){
-	red = randomColorValue();
-	green = randomColorValue();
-	blue = randomColorValue();
+	red = randomColorValue(45, 45);
+	green = randomColorValue(40, 75);
+	blue = randomColorValue(20, 160);
 	return('rgb(' + red + ', ' + green + ', ' + blue + ')');
 }
 
@@ -48,36 +45,38 @@ var Bubble = function(radius){
 	this.radius = radius;
 	this.color = randomColor();
 	this.place = function(){
-		var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");;
+		var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 		circle.setAttribute('cy', this.ypos);
 		circle.setAttribute('cx', this.xpos);
 		circle.setAttribute('r', this.radius);
 		circle.setAttribute('fill', this.color);
 		gameboard.appendChild(circle);
 	}
-	// this.remove = function(){
-	// 	console.log(this)
-	// 	// this.parentNode.removeChild(this);
-	// }
 }
 
 function makeBubbles(num){
 	var bubbleList = new Array;
 	for (var i = 0; i < (num + 1); i++){
+		//increase the bubble's radius by 2 pixels for each new bubble
 		bubbleList[i] = new Bubble(i * 2 + 10);
 		bubbleList[i].place();
-		// bubbleList[i].setAttribute('id', i);
-		// // thisbubble = bubbleList[i];
-		// document.getElementById(i).on('click', bubbleList[i].remove());
 	}
 	console.log(bubbleList)
 }
 
-makeBubbles(10);
-circleList = document.getElementById('gameboard').childNodes;
-for (var i = 0; i < circleList.length; i++){
-	circleList[i].addEventListener('click', function(){
-		this.parentNode.removeChild(this);
-		countCircles();
-	})
+//remove circle when clicked, then check to see if it was the last circle
+function setCircleClicksteners() {
+	circleList = gameboard.childNodes;
+	for (var i = 0; i < circleList.length; i++){
+		circleList[i].addEventListener('click', function(){
+			this.parentNode.removeChild(this);
+			countCircles();
+		})
+	}
 }
+
+function gameStart(){
+	makeBubbles(10);
+	setCircleClicksteners();
+}
+
